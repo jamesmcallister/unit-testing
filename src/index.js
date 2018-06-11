7635a6cccc53809dd2a67354ea3f66c50f7a3c40
James McAllister <jrfmcallister@gmail.com>
2018-06-11 16:15:57 +0100

    dome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-function add(a, b){
+function add(a, b) {
   return a + b;
 }
 
-exports.add = add;
+function longestString(strings) {
+  const result = strings.sort(function(a, b) {
+    return a.length - b.length;
+  });
+  return result[result.length - 1];
+}
+
+module.exports = {
+  add,
+  longestString
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,12 @@
-const functions = require('../src/index');
+const { add, longestString } = require("../src/index.js");
 
-test('Addition', function(){
+test("Addition", function() {
   const expected = 5;
-  const result = functions.add(2, 3);
+  const result = add(2, 3);
   expect(result).toBe(expected);
+});
+
+test("longestString", () => {
+  const strings = ["a", "adfdsfaf", "asd"];
+  expect(longestString(strings)).toBe("adfdsfaf");
 });
