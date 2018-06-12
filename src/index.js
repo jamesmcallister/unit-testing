7d36b73aa2cdf9f9ac4feae96080868b0aacf925
James McAllister <jrfmcallister@gmail.com>
2018-06-12 10:32:11 +0100

    about time we commited

--- src/index.js
+++ src/index.js
@@ -3,13 +3,50 @@ function add(a, b) {
 }
 
 function longestString(strings) {
-  const result = strings.sort(function(a, b) {
-    return a.length - b.length;
-  });
+  const result = strings.sort((a, b) => a.length - b.length);
   return result[result.length - 1];
 }
+const formula = {
+  i: 1,
+  l: 1,
+  z: 2,
+  e: 3,
+  a: 4,
+  s: 5,
+  g: 6,
+  t: 7,
+  y: 7,
+  b: 8,
+  q: 9,
+  o: 0
+};
+
+function Leet(string) {
+  const result = [...string].map(
+    item => (typeof formula[item] === "number" ? formula[item] : item)
+  );
+  return result.join("");
+}
+
+function uniqueStrings(arrayOfStrings) {
+  const result = arrayOfStrings.reduce((acc, item) => {
+    return { ...acc, [item]: 1 };
+  }, {});
+  return Object.keys(result);
+}
+
+function Developer(name, languages) {
+  this.name = name;
+  this.languages = languages;
+}
+Developer.prototype.learnLanguage = function(string) {
+  return (this.languages = uniqueStrings([...this.languages, string]));
+};
 
 module.exports = {
   add,
-  longestString
+  longestString,
+  Leet,
+  uniqueStrings,
+  Developer
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,10 @@
-const { add, longestString } = require("../src/index.js");
+const {
+  add,
+  longestString,
+  Leet,
+  uniqueStrings,
+  Developer
+} = require("../src/index.js");
 
 test("Addition", function() {
   const expected = 5;
@@ -9,4 +15,27 @@ test("Addition", function() {
 test("longestString", () => {
   const strings = ["a", "adfdsfaf", "asd"];
   expect(longestString(strings)).toBe("adfdsfaf");
+});
+
+test("1337", function() {
+  expect(Leet("lollie")).toBe("101113");
+  expect(Leet("alex")).toBe("413x");
+});
+
+test("l337", function() {
+  const expected = "413x";
+  const result = Leet("alex");
+  expect(result).toBe(expected);
+});
+
+test("uniqueStrings", function() {
+  const strings = ["beep", "clang", "keplunk", "clang", "beep", "moo"];
+  expect(uniqueStrings(strings)).toEqual(["beep", "clang", "keplunk", "moo"]);
+});
+
+test("devloper", function() {
+  const devEdem = new Developer("Edem", ["js", "html", "css"]);
+  devEdem.learnLanguage("java");
+  devEdem.learnLanguage("java");
+  expect(devEdem.languages).toEqual(["js", "html", "css", "java"]);
 });
